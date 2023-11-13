9abb28bc56a308ca8817a11a4a029dc3605ae369
alinageng <geng.a@northeastern.edu>
2023-11-13 17:36:29 -0500

    started modules

--- courses/routes.js
+++ courses/routes.js
@@ -1,5 +1,4 @@
 import Database from "../Database/index.js";
-// import Database from "../../kanbas-react-web-app/src/Kanbas/Database/index.js";
 
 function CourseRoutes(app) {
 
@@ -15,10 +14,35 @@ function CourseRoutes(app) {
   });
   app.delete("/api/courses/:id", (req, res) => {
     const { id } = req.params;
+    console.log("here");
+    console.log("deleting course : " + req.params.id);
+
     Database.courses = Database.courses
       .filter((c) => c._id !== id);
     res.sendStatus(204);
   });
+  app.put("/api/courses/:id", (req, res) => {
+    const { id } = req.params;
+    const course = req.body;
+    Database.courses = Database.courses.map((c) =>
+      c._id === id ? { c, ...course } : c
+    );
+    res.send(course);
+    // res.sendStatus(204);
+  });
+  app.get("/api/courses/:id", (req, res) => {
+    const { id } = req.params;
+    const course = Database.courses
+      .find((c) => c._id === id);
+    if (!course) {
+      res.status(404).send("Course not found");
+      return;
+    }
+    res.send(course);
+  });
+
+
+
 
 
 }
