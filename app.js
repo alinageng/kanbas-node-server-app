3db5a708646ea065b03c4341ad05861d5cc83069
alinageng <geng.a@northeastern.edu>
2023-11-14 18:01:33 -0500

    trying to fix cors issues

--- app.js
+++ app.js
@@ -5,14 +5,15 @@ import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import "dotenv/config";
-const allowedOrigins = [process.env.FRONTEND_URL, "https://a5--spontaneous-entremet-bf69b2.netlify.app/"];
+// const allowedOrigins = [process.env.FRONTEND_URL, "https://a5--spontaneous-entremet-bf69b2.netlify.app/"];
 const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: allowedOrigins
+    origin: process.env.FRONTEND_URL
   })
 );
+app.use(cors());
 app.use(express.json());
 ModuleRoutes(app);
 CourseRoutes(app);
